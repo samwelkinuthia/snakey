4a93443e155a5e0d3afb500d6473d905d142ccba
samwel <samwuelkinuthia@gmail.com>
2017-11-16 16:41:46 +0300

    update

---
+++ samples/selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(n):
+    first=0
+    while first != len(n):
+        for i in range(first, len(n)):
+            if n[i] < n[first]:
+                n[first], n[i] = n[i], n[first]
+        first += 1
+    return n
+
+
+print(selection_sort([1, 10, 5, 20, 56, 2193, 19328, 435, 12, 45, 76, 14, 45]))
