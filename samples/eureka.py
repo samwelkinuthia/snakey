1017097ec459aeeb33042f24a0a40bcbea9995b3
samwel <samwuelkinuthia@gmail.com>
2017-12-10 10:13:14 +0300

    returned eureka numbers

---
+++ samples/eureka.py
@@ -0,0 +1,17 @@
+import math
+def eureka(x, y):
+    out=[]
+    for i in range(x,y+1):
+        i = str(i)
+        if len(i) == 1:
+            out.append(int(i))
+        if len(i) > 1:
+            total = 0
+            if len(i) == 2:
+                total = int(math.pow(int(i[0]), 1) + math.pow(int(i[1]), 2))
+            elif len(i) == 3:
+                total = int(math.pow(int(i[0]), 1) + math.pow(int(i[1]), 2) + math.pow(int(i[2]), 3))
+            if str(total) == i:
+                out.append(int(total))
+    return out
+print(eureka(89,135))
